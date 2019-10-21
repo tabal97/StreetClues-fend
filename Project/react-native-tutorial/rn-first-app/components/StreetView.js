d9e1b1e5959ac41343e5af45caf48df35c9eb045
Samuel Tatman <samueltatman@Samuels-MacBook-Pro.local>
2019-10-21 14:37:55 +0100

    do streetview

--- Project/react-native-tutorial/rn-first-app/components/StreetView.js
+++ Project/react-native-tutorial/rn-first-app/components/StreetView.js
@@ -1,14 +1,44 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { WebView } from "react-native-webview";
 
 class StreetView extends Component {
-    render() {
-        return (
-            <View>
-                <Text>StreetView</Text>
-            </View>
-        );
-    }
+  render() {
+    coordinates = [-30.7229747, 25.0958533];
+    return (
+      <View
+        style={{
+          width: "100%",
+          height: "120%",
+          display: "flex",
+          marginTop: "-10%",
+          justifyContent: "center"
+        }}
+      >
+        <WebView
+          source={{
+            uri: `https://www.google.com/maps/@?api=1&map_action=pano&viewpoint=${
+              coordinates[0]
+            },${coordinates[1]}`
+          }}
+          scalesPageToFit={true}
+        />
+      </View>
+    );
+  }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "space-between"
+  },
+  video: {
+    marginTop: 20,
+    maxHeight: 200,
+    width: 320
+  }
+});
 
 export default StreetView;
