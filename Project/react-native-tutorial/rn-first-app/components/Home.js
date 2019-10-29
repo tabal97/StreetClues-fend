a49fde8abb642b88830fc28e247c293e94a29325
Mohamed <mr.tabal@yahoo.co.uk>
2019-10-29 10:37:46 +0000

    changed buttons to touchable opcities and completed styling

--- Project/react-native-tutorial/rn-first-app/components/Home.js
+++ Project/react-native-tutorial/rn-first-app/components/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { StyleSheet, ImageBackground, View, Button, Image } from "react-native";
+import { StyleSheet, ImageBackground, View, Image, TouchableOpacity, Text } from "react-native";
 import axios from "axios";
 import { convertArea } from "geolib";
 
@@ -10,10 +10,10 @@ class Home extends Component {
   render() {
     return (
       <ImageBackground source={require("../assets/background-home.jpg")} style={styles.container} >
-        <View >
+        <View style={styles.container}>
           <Image source={require("../assets/logo-transparent.png")} style={styles.img} />
-          <Button title="Create Game" onPress={this.handleCreateGame} />
-          <Button title="Join Game" onPress={this.handleJoinGame} />
+          <TouchableOpacity onPress={this.handleCreateGame} ><Text style={styles.create}>Create Game</Text></TouchableOpacity>
+          <TouchableOpacity onPress={this.handleJoinGame} ><Text style={styles.join}>Join Game</Text></TouchableOpacity>
         </View></ImageBackground >
     );
   }
@@ -38,7 +38,6 @@ class Home extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "whitesmoke",
     alignItems: "center",
     justifyContent: "space-between"
   },
@@ -46,6 +45,23 @@ const styles = StyleSheet.create({
     width: 350,
     height: 350,
     resizeMode: 'contain'
+  },
+  create: {
+    fontSize: 30,
+    backgroundColor: "whitesmoke",
+    opacity: 0.7,
+    borderRadius: 10,
+    overflow: "hidden",
+    padding: 10
+  },
+  join: {
+    fontSize: 30,
+    backgroundColor: "whitesmoke",
+    opacity: 0.8,
+    marginBottom: 100,
+    borderRadius: 10,
+    overflow: "hidden",
+    padding: 10
   }
 });
 
